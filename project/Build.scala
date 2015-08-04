6ae3dd2086ca053e7d3428529a33c513b14251e1
Longyang Zhang <longyang.zhang@rea-group.com>
2015-08-04 17:51:39 +0800

    move sbt-rest-rpc into project/

--- project/Build.scala
+++ project/Build.scala
@@ -6,5 +6,5 @@ object MultipleModuleProjectBuild extends Build {
   lazy val sample = Project(id = "rest-rpc-sample", base = file("rest-rpc-sample")) dependsOn(plugin, play)
   lazy val play = Project(id = "rest-rpc-play", base = file("rest-rpc-play")) dependsOn(core)
   lazy val core = Project(id = "rest-rpc-core", base = file("rest-rpc-core"))
-  lazy val plugin = Project(id = "sbt-rest-rpc", base = file("sbt-rest-rpc"))
+  lazy val plugin = Project(id = "sbt-rest-rpc", base = file("project/sbt-rest-rpc"))
 }

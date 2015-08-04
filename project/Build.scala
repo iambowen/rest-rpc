2e0bf622a063c96581b7a51e202dc2dc1dd2610b
Longyang Zhang <longyang.zhang@rea-group.com>
2015-08-04 13:32:11 +0800

    add submodule with the definition of their dependencies

---
+++ project/Build.scala
@@ -0,0 +1,10 @@
+import sbt._
+
+object MultipleModuleProjectBuild extends Build {
+  lazy val root = Project(id = "rest-rpc", base = file(".")) aggregate(sample)
+
+  lazy val sample = Project(id = "rest-rpc-sample", base = file("rest-rpc-sample")) dependsOn(plugin, play)
+  lazy val play = Project(id = "rest-rpc-play", base = file("rest-rpc-play")) dependsOn(core)
+  lazy val core = Project(id = "rest-rpc-core", base = file("rest-rpc-core"))
+  lazy val plugin = Project(id = "sbt-rest-rpc", base = file("sbt-rest-rpc"))
+}
